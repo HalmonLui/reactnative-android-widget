a88d6557868d21f1a3e8949af48f6e93bf71cdcc
Lui <luih@wit.edu>
2021-03-17 20:31:48 -0400

    add text input and title

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{padding: 40, fontSize: 20}}>React Native Widget Trial</Text>
+      <View style={styles.inputContainer}>
+        <Text style={{padding: 20}}>Widget Text:</Text>
+        <TextInput
+          style={{
+          height: 40,
+          width: 150,
+          borderColor: 'gray',
+          borderWidth: 1
+          }}
+          defaultValue="test"></TextInput>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +29,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'lightblue',
+    width: 300,
+    padding: 10,
+  }
 });
